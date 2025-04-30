faefa8d571066ef7df29fe7cb5e4773bd69ebf57
Nicky <nickysimanullang123@gmail.com>
2025-04-30 20:41:05 +0700

    perbaikan

--- resources/views/galeri.blade.php
+++ resources/views/galeri.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Galeri Kegiatan - HKBP Sinambela Simanullang</title>
+    <link rel="  website icon" href="{{ asset('images/hkbplogo.png') }}" type="image/png">
+
 
     {{-- Bootstrap & Icons --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
