6d9e64d7b33c1180a7cb97e7f779298b36c50a77
ramadhani1s <ramadhanisihotang4@gmail.com>
2025-06-02 16:15:14 +0700

    perubahan edit jemaat

--- resources/views/admin/jemaat/edit.blade.php
+++ resources/views/admin/jemaat/edit.blade.php
@@ -86,7 +86,7 @@
                         <a href="{{ route('admin.jemaat.index') }}" class="btn btn-secondary">
                             Kembali
                         </a>
-                        <button type="submit" class="btn text-white" style="background-color: #0D99FF;" form="formBerita" fdprocessedid="ocidwx">
+                        <button type="submit" class="btn text-white" style="background-color: #0D99FF;">
                             Edit
                         </button>
                     </div>
